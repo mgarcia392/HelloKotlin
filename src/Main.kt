ba5053a0d7c99c022d797eea77a2c1cac16dc60e
Michael Garcia <mgarc287@ucsc.edu>
2024-09-06 16:01:37 -0700

    Hello Kotlin file

---
+++ src/Main.kt
@@ -0,0 +1,20 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    var name = "Kotlin"
+
+    println("Hello Kotlin")
+
+
+    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    // to see how IntelliJ IDEA suggests fixing it.
+    name = "Michael"
+    println("Hello, $name!")
+
+    val x = 5
+    val y = 8
+    var sum = x+y
+
+    println("$x + $y = $sum")
+
+}
